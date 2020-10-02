9aecd59a7e59aef9dafd8e837341218a76cdfc1f
JeongWon Liee <khjvvv7@gmail.com>
2020-10-02 15:44:34 +0900

    전통적인 방식의 스프링 controller

---
+++ webflux-begin/src/main/java/lee/garden/webflux/HelloWorld.java
@@ -0,0 +1,15 @@
+package lee.garden.webflux;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloWorld {
+
+    private Long id;
+    private String name;
+
+}
